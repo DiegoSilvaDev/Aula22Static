c078490104010c08bf3be8456fff2568ab8e4286
DiegoSilvaDev <Diego2102.ds@gmail.com>
2020-06-22 14:46:39 -0300

    ok

---
+++ Conversor.cs
@@ -0,0 +1,21 @@
+namespace Static
+{
+    public static class Conversor
+    {
+        public static float CotacaoDolar = 5.23f;
+        public static float CotacaoEuro = 5.90f;
+
+        public static float ConverterDolarParaReal(float valor){
+            return valor * CotacaoDolar;
+        }
+         public static float ConverterRealParaDolar(float valor){
+            return valor / CotacaoDolar;
+        }
+         public static float ConverterEuroParaReal(float valor){
+            return valor * CotacaoEuro;
+        }
+         public static float ConverterRealParaEuro(float valor){
+            return valor / CotacaoEuro ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            
+            Console.WriteLine(Conversor.ConverterDolarParaReal(200));
+            Console.WriteLine(Conversor.ConverterRealParaDolar(200));
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(Conversor.ConverterEuroParaReal(250));
+            Console.WriteLine(Conversor.ConverterRealParaEuro(250));
+
+            Console.ResetColor();
+        }
+    }
+}
